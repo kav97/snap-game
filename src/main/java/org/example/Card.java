528fb7051ece919d3a10e363994eef5852f26995
Kavandeep <kavan1@live.co.uk>
2022-08-19 11:14:15 +0100

    can get all 52 cards

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class Card {
+
+    private final String suit;
+
+    private final String symbol;
+
+    private final int value;
+
+    public Card ( String suit, String symbol, int value ) {
+
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Symbol: %s\nSuit: %s\nValue: %d\n", getSymbol(), getSuit(), getValue());
+    }
 }
